fdff563b85adc2db8440f84dd987f3adbe92d935
bbamsu99 <skqlgyrhk89@naver.com>
2023-06-16 19:28:14 +0900

    일단 저장합니다

--- front/views.py
+++ front/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
 # Create your views here.
 
 def main(request):
-
     return render(request, 'front/main.html')
+
+# def main(request):
+#
+#     return render(request, 'front/main.html')
