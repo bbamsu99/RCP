925be7eccc31cd50885869464e585781afa990f6
bbamsu99 <skqlgyrhk89@naver.com>
2023-06-16 19:38:46 +0900

    찐막

--- front/views.py
+++ front/views.py
@@ -1,16 +1,7 @@
-from django.shortcuts import render, redirect
-
-from Body.models import Post
+from django.shortcuts import render
 
 
 # Create your views here.
 
 def main(request):
-
-    post = Post.objects.all()
-
     return render(request, 'front/main.html')
-
-# def main(request):
-#
-#     return render(request, 'front/main.html')
