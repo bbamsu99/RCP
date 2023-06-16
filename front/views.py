93acf5f5b7b5c489d20fd2e4b0d9c380856dd43f
bbamsu99 <skqlgyrhk89@naver.com>
2023-06-16 19:37:26 +0900

    된다 돼 근데이제 못바꾸겠다

--- front/views.py
+++ front/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from Body.models import Post
+
+
 # Create your views here.
 
 def main(request):
+
+    post = Post.objects.all()
+
     return render(request, 'front/main.html')
 
 # def main(request):
